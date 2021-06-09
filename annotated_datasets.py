b1e3811c3d04f2012792c9fa60f84485bb4b2fdc
AndreHe02 <andre.he@outlook.com>
2021-06-09 22:37:24 +0800

    .

--- annotated_datasets.py
+++ annotated_datasets.py
@@ -47,3 +47,36 @@ class SPBoWDataset(Dataset):
     def __getitem__(self, idx):
         ant = self.ants[idx]
         return self._get_seven_planes(ant['sgf']), self.get_bag_of_words(ant['comments'])
+    
+class LengthDataset(Dataset):
+    def __init__(self, annotations, thresh = 76):
+        self.ants = annotations
+        self.thresh = thresh
+    
+    @staticmethod
+    def _get_seven_planes(sgf_str):
+        game = goboard.GameState.new_game(19)        
+        move_seq = sgf_str.split(';')[1:-1]
+        for move_name in move_seq:
+            idx = move_name.index('[')
+            col_s, row_s = move_name[idx+1], move_name[idx+2]
+            col = ord(col_s) - 97
+            row = 19 - ord(row_s) + 96
+            if 'B' in move_name:
+                game.board.place_stone(Player.black, Point(row+1, col+1))
+            elif 'W' in move_name:
+                game.board.place_stone(Player.white, Point(row+1, col+1))
+        encoder = SevenPlaneEncoder((19, 19))
+        svp = encoder.encode(game)
+        ones = np.ones((1, 19, 19))
+        return np.concatenate((svp, ones), axis=0)
+    
+    def __len__(self):
+        return len(self.ants)
+
+    def __getitem__(self, idx):
+        ant = self.ants[idx]
+        return self._get_seven_planes(ant['sgf']), int(len(ant['comments']) > self.thresh)
+    
+    
+        
